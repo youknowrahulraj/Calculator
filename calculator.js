68b681df5f75de5583eb32c3e5ebcdbbc05f5f43
Rahul Raj <youknowrahulraj@gmail.com>
2026-05-26 15:27:16 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,23 +5,42 @@ let expression = "";
 
 buttons.forEach(button => {
     button.addEventListener("click", () => {
+
         let value = button.innerText;
 
+        // Clear screen
         if (value === "Clear") {
             expression = "";
             screen.innerText = "";
-        } 
+        }
+
+        // Calculate
         else if (value === "=") {
+
             try {
-                expression = eval(expression);
-                screen.innerText = expression;
+
+                // safer than eval
+                let result = Function('"use strict"; return (' + expression + ')')();
+
+                screen.innerText = result;
+                expression = result.toString();
+
             } catch {
+
                 screen.innerText = "Error";
+                expression = "";
+
             }
-        } 
+
+        }
+
+        // Add values
         else {
+
             expression += value;
             screen.innerText = expression;
+
         }
+
     });
 });
